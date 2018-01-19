c4b5817444f39a6f97773cc2fb35bc276bd5e206
Rock3f <tanguy.badier@ynov.com>
2018-01-19 14:06:41 +0100

    Fix Last Merge homePageUX on master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { BarreFooterComponent } from './barre-footer/barre-footer.component';
-=======
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material';
 import { MatInputModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
->>>>>>> homePageUX
 
 
 @NgModule({
